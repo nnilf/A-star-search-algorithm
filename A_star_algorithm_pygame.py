34d352f37bcd9f8c3b9f688cc1fe756aaf4de3a2
Flinn Mort <flinnmort13@outlook.com>
2025-04-08 20:57:16 +0100

    created heuristic function and added f,g,h scores to quadrants

--- A_star_algorithm_pygame.py
+++ A_star_algorithm_pygame.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Quadrant:
     def __init__(self, x, y):
         self._x = x
@@ -5,6 +7,9 @@ class Quadrant:
         self._start = False
         self._end = False
         self._barrier = False
+        self._g_score = np.inf
+        self._h_score = np.inf
+        self._f_score = np.inf
 
     def get_coords(self):
         return (self._x, self._y)
@@ -12,6 +17,9 @@ class Quadrant:
     def set_start(self):
         if not(self._end):
             self._start = True
+            self._g_score = 0
+            self._h_score = 0
+            self._f_score = 0
         else:
             return "Already end node"
     
@@ -40,6 +48,23 @@ class Quadrant:
         self._start = False
         self._end = False
         self._barrier = False
+
+    def get_children(self, grid):
+        children = []
+
+def heuristic(end, quad):
+    """
+    Calculates the heuristic Fscore for the selected quadrant
+
+    :param start: starting node
+    :param end: ending node
+    :param quad: node for calculations to applied from
+    :returns: Fscore, Hscore, Gscore from calculations
+    """
+    quad_x, quad_y = quad.get_coords()
+    end_x, end_y = end.get_coords()
+    return (abs(quad_x - end_x) + abs(quad_y - end_y))
+
         
 def create_grid():
     grid = []

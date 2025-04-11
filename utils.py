5081322c28b0b8492ac209aa035dcde61263b311
Flinn Mort <flinnmort13@outlook.com>
2025-04-11 14:05:03 +0100

    further utils updates

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import pygame
 from constants import Colors
 from Node import Node
 
+
 def create_grid(grid_size: int, width: int, difference: int) -> list[list[Node]]:
     """Intialises a grid of node classes"""
     gap = width // grid_size
@@ -13,11 +14,13 @@ def create_grid(grid_size: int, width: int, difference: int) -> list[list[Node]]
             grid[i].append(Node(i, j, gap, grid_size, difference))
     return grid
 
+
 def heuristic(node_coords: tuple[int, int], end_coords: tuple[int, int]) -> float:
     """Calculates the heuristic Fscore for the selected node using Euclidean distance"""
     node_x, node_y = node_coords
     end_x, end_y = end_coords
     return math.sqrt((node_x - end_x) ** 2 + (node_y - end_y) ** 2)
+
 
 def get_clicked_pos(pos: tuple, rows: int, width: int, difference: int) -> tuple[int, int]:
     """Gets the row and column of where the mouse has clicked"""
@@ -42,6 +45,7 @@ def draw_grid(rows: int, width: int, win: pygame.Surface, difference: int):
         pygame.draw.line(win, Colors.GREY, (0, (i * gap) + difference), (width, (i * gap) + difference))
         for j in range(rows):
             pygame.draw.line(win, Colors.GREY, (j * gap, difference), (j * gap, width+difference))
+
 
 def clear_path_and_update_children(grid, grid_size, start, end):
     """Clear visualization nodes and update node connections"""
